c50247a9abb85d47ed3c9001053d7e5bb9107fea
Bruno Arueira <bgnarueira@gmail.com>
2011-08-07 10:12:29 -0300

    Config database with mongodb

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,5 +1,11 @@
-MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
-MongoMapper.database = "#example-#{Rails.env}"
+db_config = YAML::load(File.read(RAILS_ROOT + "/config/database.yml"))
+
+if db_config[Rails.env] && 
+db_config[Rails.env]['adapter'] == 'mongodb'
+  mongo = db_config[Rails.env]
+  MongoMapper.connection = Mongo::Connection.new(mongo['hostname'])
+  MongoMapper.database = mongo['database']
+end
 
 if defined?(PhusionPassenger)
    PhusionPassenger.on_event(:starting_worker_process) do |forked|
